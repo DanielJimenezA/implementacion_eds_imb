818454e9b6c50841833778d5c808bf4d39ec307f
lAbEl JD <83861199+DanielJimenezA@users.noreply.github.com>
2026-07-06 11:57:18 -0600

    style: reemplaza OpenStreetMap por CARTO Positron

--- js/app.js
+++ js/app.js
@@ -184,9 +184,19 @@ function inicializarMapa() {
     maxBoundsViscosity: 1.0,
   });
 
-  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution: "&copy; OpenStreetMap",
-  }).addTo(mapa);
+  // L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  //   attribution: "&copy; OpenStreetMap",
+  // }).addTo(mapa);
+
+  L.tileLayer(
+    "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png",
+    {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+      subdomains: "abcd",
+      maxZoom: 20,
+    }
+  ).addTo(mapa);
 
   mapa.fitBounds(mexicoBounds);
 }
